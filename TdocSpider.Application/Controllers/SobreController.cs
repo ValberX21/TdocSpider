a364a0e4da1f48d982c0de86767360b5a93058b2
Valber Martins <martins_shinigami@hotmail.com>
2025-07-10 18:30:46 -0300

    Criada pagina 'SOBRE'
    Projeto refeito devido à estrutura incorreta da solução anterior, que impedia a adição do projeto de testes automatizados.

---
+++ TdocSpider.Application/Controllers/SobreController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using TdocSpider.Application.Models.ViewModels;
+
+namespace TdocSpider.Application.Controllers
+{
+    public class SobreController : Controller
+    {
+        public IActionResult Index()
+        {
+            SobreViewModel dtSistema = new SobreViewModel
+            {
+                NomeSistema = "DocSpider",
+                Versao = "1.0.0",
+                DataAtualizacao = DateTime.Now.ToString(),
+                Desenvolvedor = "Valber",
+                MensagemRodape = "Este sistema é uma demonstração com dados fictícios.",
+                LogoUrl = Url.Content("~/images/logo.png")
+            };
+
+            return View(dtSistema);
+        }
+    }
+}
---
+++ TdocSpider.Application/Models/ViewModels/SobreViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TdocSpider.Application.Models.ViewModels
+{
+    public class SobreViewModel
+    {
+        public string NomeSistema { get; set; }
+        public string Versao { get; set; }
+        public string DataAtualizacao { get; set; }
+        public string Desenvolvedor { get; set; }
+        public string MensagemRodape { get; set; }
+        public string LogoUrl { get; set; }
+    }
+}
